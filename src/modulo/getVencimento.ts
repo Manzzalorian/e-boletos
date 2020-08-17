181ab08a5bc33474af3fe21435e437d5a4690509
manzza <renato@winn.com.vc>
2020-08-17 13:07:22 -0300

    fix fator tests

--- src/modulo/getVencimento.ts
+++ src/modulo/getVencimento.ts
@@ -1,6 +1,6 @@
 export function getVencimento(fator: number): string {
     if (fator == 0) return "data de vencimento não definida"
     let date: Date = new Date("1997-10-07");
-    date.setDate(date.getDate() + fator + 1);
-    return date.toLocaleDateString();
+    date.setDate(date.getDate() + fator);
+    return date.toISOString().slice(0,10);//YYYY-MM-DD
 }
--- src/test/fator.test.ts
+++ src/test/fator.test.ts
@@ -3,7 +3,7 @@ import { getVencimento } from "../modulo/getVencimento";
 it('DATA X FATOR ', () => {
 
     const FATOR = [1000, 1001];
-    const DV = ["7/3/2000", "7/4/2000", ];
+    const DV = ["2000-07-03", "2000-07-04" ];
     
     DV.forEach((_, i) => {
         expect(getVencimento(FATOR[i])).toEqual(DV[i]);
